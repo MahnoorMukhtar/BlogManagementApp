7d03c0b8b91313180f1352358da2c3c49cbcb35d
MahnoorMukhtar <mahnoormukhtar519@gmail.com>
2026-01-03 15:53:43 +0500

    frontend context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
-import { AuthProvider } from '../context/authContextProvider.jsx'
+import { AuthProvider } from '../context/AuthContextProvider.jsx'
 import { BlogProvider } from '../context/BlogContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
